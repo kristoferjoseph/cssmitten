ed49a49f272cbc704f8fd7c3709784eb822a42b9
kj <kristoferjoseph@gmail.com>
2018-05-22 15:22:46 -0700

    Adds display:inline

--- display.js
+++ display.js
@@ -2,6 +2,7 @@ module.exports = function display (query) {
   return `/* DISPLAY */
 .d-none${query}{display:none;}
 .d-block${query}{display:block;}
+.d-inline${query}{display:inline;}
 .d-inline-block${query}{display:block;}
 .d-flex${query}{display:flex;}
 .d-inline-flex${query}{display:inline-flex;}
