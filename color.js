af58ec181673c3dba7d8b273397f77bb9bf4c0e4
kj <kristoferjoseph@gmail.com>
2019-01-15 10:19:07 -0800

    Updates color states.

--- color.js
+++ color.js
@@ -1,3 +1,5 @@
+var hs = require('hash-switch')
+
 module.exports = function color (state) {
   state = state || {}
   var query = state.query || ''
@@ -5,31 +7,64 @@ module.exports = function color (state) {
   var colors = config.colors
   var output = '\n/* COLOR */\n'
   var variable
-  var primary = colors.primary || []
-  var hover = colors.hover || []
-  var active = colors.active || []
-  var disabled = colors.disabled || []
+  var sm = hs({
+    'link': link,
+    'visited': visited,
+    'hover': hover,
+    'active': active,
+    'disabled': disabled
+  }, handler)
 
-  primary.forEach(function (color, i) {
-    variable = 'p' + i
-    output += `.c-${variable}${query}{color:var(--${variable});}/* ${color.label} */\n`
-  })
+  Object.keys(colors).forEach(k => sm(k, colors[k], k.charAt(0).toLowerCase()))
 
-  hover.forEach(function (color, i) {
-    variable = 'h' + i
-    output += `.c-${variable}${query}:hover{color:var(--${variable});}/* ${color.label} */\n`
-  })
+  function handler (a, n) {
+    a = a || []
+    a.forEach(function (color, i) {
+      variable = `${n}${i}`
+      output += `.c-${variable}${query}{color:var(--${variable});}/* ${color.label} */\n`
+    })
+  }
 
-  active.forEach(function (color, i) {
-    variable = 'a' + i
-    output += `.c-${variable}${query}:active{color:var(--${variable});}/* ${color.label} */\n`
-    output += `.c-${variable}${query}.active{color:var(--${variable});}/* ${color.label} */\n`
-  })
+  function link (a) {
+    a = a || []
+    a.forEach(function (color, i) {
+      variable = 'l' + i
+      output += `.c-${variable}${query}:link{color:var(--${variable});}/* ${color.label} */\n`
+    })
+  }
 
-  disabled.forEach(function (color, i) {
-    variable = 'd' + i
-    output += `.c-${variable}${query}:disabled{color:var(--${variable});}/* ${color.label} */\n`
-  })
+  function visited (a) {
+    a = a || []
+    a.forEach(function (color, i) {
+      variable = 'v' + i
+      output += `.c-${variable}${query}:visited{color:var(--${variable});}/* ${color.label} */\n`
+    })
+  }
+
+  function hover (a) {
+    a = a || []
+    a.forEach(function (color, i) {
+      variable = 'h' + i
+      output += `.c-${variable}${query}:hover{color:var(--${variable});}/* ${color.label} */\n`
+    })
+  }
+
+  function active (a) {
+    a = a || []
+    a.forEach(function (color, i) {
+      variable = 'a' + i
+      output += `.c-${variable}${query}:active{color:var(--${variable});}/* ${color.label} */\n`
+      output += `.c-${variable}${query}.active{color:var(--${variable});}/* ${color.label} */\n`
+    })
+  }
+
+  function disabled (a) {
+    a = a || []
+    a.forEach(function (color, i) {
+      variable = 'd' + i
+      output += `.c-${variable}${query}:disabled{color:var(--${variable});}/* ${color.label} */\n`
+    })
+  }
 
   return output
 }
--- typeface.js
+++ typeface.js
@@ -2,11 +2,14 @@ module.exports = function typeface (state) {
   state = state || {}
   var config = state.config || {}
   var base = config.base || 16
+  var ratio = config.ratio || 1.333
+  var font = config.font || '-apple-system, BlinkMacSystemFont, sans-serif'
   return `/* TYPEFACE */
 html {font-size: ${base}px;}
 body {
+  font-family: ${font};
   font-weight: normal;
-  line-height: 1.333;
+  line-height: ${ratio};
   text-rendering: geometricPrecision;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
