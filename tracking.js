ef7214c3cc49818c49b3542fb62c276928e72e7e
kj <kristoferjoseph@gmail.com>
2020-06-30 11:25:06 -0700

    Updates output.

--- tracking.js
+++ tracking.js
@@ -4,7 +4,7 @@ module.exports = function tracking(query) {
 .tracking3${query}{letter-spacing: 0.1em;}
 .tracking2${query}{letter-spacing: 0.05em;}
 .tracking1${query}{letter-spacing: 0.025em;}
-.tracking0${query}{query}{letter-spacing: 0;}
+.tracking0${query}{letter-spacing: 0;}
 .tracking-1${query}{letter-spacing: -0.025em;}
 .tracking-2${query}{letter-spacing: -0.05em;}
   `
